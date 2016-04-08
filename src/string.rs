658401578d3b82eb2fac2b83dbf09d679f94025b
Daniel P. Clark <6ftdan@gmail.com>
2016-04-08 07:26:27 -0400

    Working string justification.

--- src/string.rs
+++ src/string.rs
@@ -12,46 +12,53 @@ pub mod string {
     ///
     /// # Example
     /// ```
+    /// use array_tool::string::Justify;
+    ///
+    /// "asd asdf asd".justify_line(14);
     /// ```
     ///
     /// # Output
     /// ```text
+    /// "asd  asdf  asd"
     /// ```
-    fn justify_line(&self, width: usize) -> &Self;
+    fn justify_line(&self, width: usize) -> String;
   }
 
   impl Justify for &'static str {
-    fn justify_line(&self, width: usize) -> &Self {
+    fn justify_line(&self, width: usize) -> String {
       let trimmed = self.trim() ;
-      let mut len = trimmed.len();
-      if len >= width { return self; };
+      let len = trimmed.len();
+      if len >= width { return self.to_string(); };
       let difference = width - len;
-      let mut iter = trimmed.split_whitespace();
+      let iter = trimmed.split_whitespace();
       let spaces = iter.count() - 1;
-      if spaces == 0 { return self; }
+      let mut iter = trimmed.split_whitespace().peekable();
+      if spaces == 0 { return self.to_string(); }
       let mut obj = String::with_capacity(width);
       
-      let mut div = difference / spaces;
+      let div = difference / spaces;
       let mut remainder = difference % spaces;
-
+      
       loop {
         match iter.next() {
           Some(x) => {
-            obj.push( x );
+            obj.push_str( x );
             let val = if remainder > 0 {
               remainder = remainder - 1;
               div + 1
             } else { div };
-            for _ in 0..val {
-              if iter.count() != 0 { // Don't add spaces if last word
-                obj.push( " " );
+            for _ in 0..val+1 {
+              match &iter.peek() { // Don't add spaces if last word
+                &Some(_) => {
+                  obj.push_str( " " );
+                },
+                &None => {},
               }
             }
           },
           None => { break },
         }
       }
-
       obj
     }
   }
--- tests/string.rs
+++ tests/string.rs
@@ -1,8 +1,14 @@
 extern crate array_tool;
 
 #[test]
-fn it_justifies_one_line_in_type_string() {
+fn it_justifies_one_line_in_for_string() {
   use array_tool::string::Justify;
 
   assert_eq!("asd asdf asd".justify_line(14), "asd  asdf  asd");
+  assert_eq!("asd asdf asd".justify_line(16), "asd   asdf   asd");
+  assert_eq!("asd as df asd".justify_line(16), "asd  as  df  asd");
+  assert_eq!("asd as df asd".justify_line(18), "asd   as   df  asd");
+  assert_eq!("  asd as df asd  ".justify_line(16), "asd  as  df  asd");
+  assert_eq!("asdasdfasd".justify_line(16), "asdasdfasd");
+  assert_eq!("asdasdfasd".justify_line(6), "asdasdfasd");
 }

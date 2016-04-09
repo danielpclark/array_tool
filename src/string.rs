2094a40cf99c8fd66a024467fe58f0ce182b74cf
Daniel P. Clark <6ftdan@gmail.com>
2016-04-09 05:49:18 -0400

    Word wrap and indexed replacement

--- src/string.rs
+++ src/string.rs
@@ -9,7 +9,6 @@
 pub mod string {
   /// Justified
   pub trait Justify {
-    ///
     /// # Example
     /// ```
     /// use array_tool::string::Justify;
@@ -60,6 +59,77 @@ pub mod string {
         }
       }
       obj
+    }
+  }
+
+  /// Substitute string character at each marker given
+  pub trait SubstMarks {
+    /// # Example
+    /// ```
+    /// use array_tool::string::SubstMarks;
+    ///
+    /// "asdf asdf asdf".subst_marks(vec![0,5,8], "Z");
+    /// ```
+    ///
+    /// # Output
+    /// ```text
+    /// "Zsdf ZsdZ asdf"
+    /// ```
+    fn subst_marks(&self, marks: Vec<usize>, chr: &'static str) -> String;
+  }
+  impl SubstMarks for &'static str {
+    fn subst_marks(&self, marks: Vec<usize>, chr: &'static str) -> String {
+      let mut output = String::with_capacity(self.len());
+      for x in 0..self.len() {
+        if marks.contains(&x) {
+          output.push_str(chr)
+        } else {
+          output.push(self.chars().nth(x).unwrap())
+        }
+      }
+      output
+    }
+  }
+
+  /// Word wrapping
+  pub trait WordWrap {
+    ///
+    /// # Example
+    /// ```
+    /// use array_tool::string::WordWrap;
+    ///
+    /// "asd asdf asd".word_wrap(8);
+    /// ```
+    ///
+    /// # Output
+    /// ```text
+    /// "asd asdf\nasd"
+    /// ```
+    fn word_wrap(&self, width: usize) -> String;
+  }
+  impl WordWrap for &'static str {
+    #![allow(unsafe_code)]
+    fn word_wrap(&self, width: usize) -> String {
+      let mut markers = vec![];
+      fn wordwrap(t: &'static str, chunk: usize, offset: usize, mrkrs: &mut Vec<usize>) -> String {
+        let mark = unsafe {
+          t.slice_unchecked(offset,offset+chunk)
+        }.rfind(" ");
+        match mark {
+          Some(x) => {
+            if offset+chunk < t.len() {
+              mrkrs.push(offset + x)
+            };
+            wordwrap(t, chunk, offset+x+1, mrkrs)
+          },
+          None => {
+            use string::SubstMarks;
+
+            return t.subst_marks(mrkrs.to_vec(), "\n")
+          },
+        }
+      };
+      wordwrap(self, width, 0, &mut markers)
     }
   }
 }
--- tests/string.rs
+++ tests/string.rs
@@ -12,3 +12,23 @@ fn it_justifies_one_line_in_for_string() {
   assert_eq!("asdasdfasd".justify_line(16), "asdasdfasd");
   assert_eq!("asdasdfasd".justify_line(6), "asdasdfasd");
 }
+
+#[test]
+fn it_substitutes_character_at_each_indexed_point() {
+  use array_tool::string::SubstMarks;
+
+  assert_eq!("asdf asdf asdf".subst_marks(vec![0,5,8], "Z"), "Zsdf ZsdZ asdf");
+  assert_eq!("asdf asdf asdf".subst_marks(vec![0,5,8], "\n"), "\nsdf \nsd\n asdf");
+}
+
+#[test]
+fn it_word_wraps_for_string() {
+  use array_tool::string::WordWrap;
+
+  assert_eq!(
+    "01234 67 9 BC EFG IJ".word_wrap(6),
+    "01234\n67 9\nBC\nEFG IJ"
+  );
+}
+
+

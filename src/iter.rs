c0b577540c1c764c5f77235c878e0025396c1c8d
Mikolaj Figurski <meek.f80@gmail.com>
2022-05-10 00:16:11 -0400

    Run cargo fmt

--- src/iter.rs
+++ src/iter.rs
@@ -5,162 +5,183 @@ use std::iter::IntoIterator;
 #[derive(Clone, Debug)]
 #[must_use = "iterator adaptors are lazy and do nothing unless consumed"]
 pub struct ZipOption<A, B> {
-  a: A,
-  b: B,
-  // index and len are only used by the specialized version of zip
-  index: usize,
-  len: usize,
+    a: A,
+    b: B,
+    // index and len are only used by the specialized version of zip
+    index: usize,
+    len: usize,
 }
 
 /// Zips to iterators together to the longest length
 /// via Option<(Option<A>, Option<B>)>
 pub trait ZipOpt {
-  /// Zip to iterators to longest length via Option<(Option<A>, Option<B>)> results.
-  /// # Example
-  /// ```
-  /// use array_tool::iter::ZipOpt;
-  ///
-  /// let a = vec!["a","b","c", "d"];
-  /// let b = vec!["c","d"];
-  /// let mut x = a.iter().zip_option(b.iter());
-  ///
-  /// assert_eq!(x.next(), Some((Some(&"a"), Some(&"c"))));
-  /// assert_eq!(x.next(), Some((Some(&"b"), Some(&"d"))));
-  /// assert_eq!(x.next(), Some((Some(&"c"), None)));
-  /// assert_eq!(x.next(), Some((Some(&"d"), None)));
-  /// assert_eq!(x.next(), None);
-  /// ```
-  ///
-  /// # Output
-  /// ```text
-  /// vec![ "a", "b", "c", "d" ]
-  /// ```
-  fn zip_option<U>(self, other: U) -> ZipOption<Self, U::IntoIter>
-    where Self: Sized, U: IntoIterator;
+    /// Zip to iterators to longest length via Option<(Option<A>, Option<B>)> results.
+    /// # Example
+    /// ```
+    /// use array_tool::iter::ZipOpt;
+    ///
+    /// let a = vec!["a","b","c", "d"];
+    /// let b = vec!["c","d"];
+    /// let mut x = a.iter().zip_option(b.iter());
+    ///
+    /// assert_eq!(x.next(), Some((Some(&"a"), Some(&"c"))));
+    /// assert_eq!(x.next(), Some((Some(&"b"), Some(&"d"))));
+    /// assert_eq!(x.next(), Some((Some(&"c"), None)));
+    /// assert_eq!(x.next(), Some((Some(&"d"), None)));
+    /// assert_eq!(x.next(), None);
+    /// ```
+    ///
+    /// # Output
+    /// ```text
+    /// vec![ "a", "b", "c", "d" ]
+    /// ```
+    fn zip_option<U>(self, other: U) -> ZipOption<Self, U::IntoIter>
+    where
+        Self: Sized,
+        U: IntoIterator;
 }
 
 impl<I: Iterator> ZipOpt for I {
-  #[inline]
-  fn zip_option<U>(self, other: U) -> ZipOption<Self, U::IntoIter>
-    where Self: Sized, U: IntoIterator {
-
-    ZipOption::new(self, other.into_iter())
-  }
+    #[inline]
+    fn zip_option<U>(self, other: U) -> ZipOption<Self, U::IntoIter>
+    where
+        Self: Sized,
+        U: IntoIterator,
+    {
+        ZipOption::new(self, other.into_iter())
+    }
 }
 
-impl<A, B> Iterator for ZipOption<A, B> where A: Iterator, B: Iterator {
-  type Item = (Option<A::Item>, Option<B::Item>);
+impl<A, B> Iterator for ZipOption<A, B>
+where
+    A: Iterator,
+    B: Iterator,
+{
+    type Item = (Option<A::Item>, Option<B::Item>);
 
-  #[inline]
-  fn next(&mut self) -> Option<Self::Item> {
-    ZipImpl::next(self)
-  }
+    #[inline]
+    fn next(&mut self) -> Option<Self::Item> {
+        ZipImpl::next(self)
+    }
 
-  #[inline]
-  fn size_hint(&self) -> (usize, Option<usize>) {
-    ZipImpl::size_hint(self)
-  }
+    #[inline]
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        ZipImpl::size_hint(self)
+    }
 
-  #[inline]
-  fn nth(&mut self, n: usize) -> Option<Self::Item> {
-    ZipImpl::nth(self, n)
-  }
+    #[inline]
+    fn nth(&mut self, n: usize) -> Option<Self::Item> {
+        ZipImpl::nth(self, n)
+    }
 }
 
 #[doc(hidden)]
-impl<A, B> DoubleEndedIterator for ZipOption<A, B> where
-A: DoubleEndedIterator + ExactSizeIterator,
-B: DoubleEndedIterator + ExactSizeIterator,
+impl<A, B> DoubleEndedIterator for ZipOption<A, B>
+where
+    A: DoubleEndedIterator + ExactSizeIterator,
+    B: DoubleEndedIterator + ExactSizeIterator,
 {
-  #[inline]
-  fn next_back(&mut self) -> Option<(Option<A::Item>, Option<B::Item>)> {
-    ZipImpl::next_back(self)
-  }
+    #[inline]
+    fn next_back(&mut self) -> Option<(Option<A::Item>, Option<B::Item>)> {
+        ZipImpl::next_back(self)
+    }
 }
 
 #[doc(hidden)]
 trait ZipImpl<A, B> {
-  type Item;
-  fn new(a: A, b: B) -> Self;
-  fn next(&mut self) -> Option<Self::Item>;
-  fn size_hint(&self) -> (usize, Option<usize>);
-  fn nth(&mut self, n: usize) -> Option<Self::Item>;
-  fn super_nth(&mut self, mut n: usize) -> Option<Self::Item> {
-    while let Some(x) = self.next() {
-      if n == 0 { return Some(x) }
-      n -= 1;
+    type Item;
+    fn new(a: A, b: B) -> Self;
+    fn next(&mut self) -> Option<Self::Item>;
+    fn size_hint(&self) -> (usize, Option<usize>);
+    fn nth(&mut self, n: usize) -> Option<Self::Item>;
+    fn super_nth(&mut self, mut n: usize) -> Option<Self::Item> {
+        while let Some(x) = self.next() {
+            if n == 0 {
+                return Some(x);
+            }
+            n -= 1;
+        }
+        None
     }
-    None
-  }
-  fn next_back(&mut self) -> Option<Self::Item>
-    where A: DoubleEndedIterator + ExactSizeIterator,
-          B: DoubleEndedIterator + ExactSizeIterator;
+    fn next_back(&mut self) -> Option<Self::Item>
+    where
+        A: DoubleEndedIterator + ExactSizeIterator,
+        B: DoubleEndedIterator + ExactSizeIterator;
 }
 
 #[doc(hidden)]
 impl<A, B> ZipImpl<A, B> for ZipOption<A, B>
-  where A: Iterator, B: Iterator {
-  type Item = (Option<A::Item>, Option<B::Item>);
-  fn new(a: A, b: B) -> Self {
-    ZipOption {
-      a,
-      b,
-      index: 0, // unused
-      len: 0, // unused
+where
+    A: Iterator,
+    B: Iterator,
+{
+    type Item = (Option<A::Item>, Option<B::Item>);
+    fn new(a: A, b: B) -> Self {
+        ZipOption {
+            a,
+            b,
+            index: 0, // unused
+            len: 0,   // unused
+        }
     }
-  }
 
-  #[inline]
-  fn next(&mut self) -> Option<(Option<A::Item>, Option<B::Item>)> {
-    let first = self.a.next();
-    let second = self.b.next();
+    #[inline]
+    fn next(&mut self) -> Option<(Option<A::Item>, Option<B::Item>)> {
+        let first = self.a.next();
+        let second = self.b.next();
 
-    if first.is_some() || second.is_some() {
-      Some((first, second))
-    } else {
-      None
+        if first.is_some() || second.is_some() {
+            Some((first, second))
+        } else {
+            None
+        }
     }
-  }
 
-  #[inline]
-  fn nth(&mut self, n: usize) -> Option<Self::Item> {
-    self.super_nth(n)
-  }
-
-  #[inline]
-  fn next_back(&mut self) -> Option<(Option<A::Item>, Option<B::Item>)>
-    where A: DoubleEndedIterator + ExactSizeIterator,
-          B: DoubleEndedIterator + ExactSizeIterator {
-    let a_sz = self.a.len();
-    let b_sz = self.b.len();
-    if a_sz != b_sz {
-      // Adjust a, b to equal length
-      if a_sz > b_sz {
-        for _ in 0..a_sz - b_sz { self.a.next_back(); }
-      } else {
-        for _ in 0..b_sz - a_sz { self.b.next_back(); }
-      }
+    #[inline]
+    fn nth(&mut self, n: usize) -> Option<Self::Item> {
+        self.super_nth(n)
     }
-    match (self.a.next_back(), self.b.next_back()) {
-      (None, None) => None,
-      (f,s) => Some((f, s)),
+
+    #[inline]
+    fn next_back(&mut self) -> Option<(Option<A::Item>, Option<B::Item>)>
+    where
+        A: DoubleEndedIterator + ExactSizeIterator,
+        B: DoubleEndedIterator + ExactSizeIterator,
+    {
+        let a_sz = self.a.len();
+        let b_sz = self.b.len();
+        if a_sz != b_sz {
+            // Adjust a, b to equal length
+            if a_sz > b_sz {
+                for _ in 0..a_sz - b_sz {
+                    self.a.next_back();
+                }
+            } else {
+                for _ in 0..b_sz - a_sz {
+                    self.b.next_back();
+                }
+            }
+        }
+        match (self.a.next_back(), self.b.next_back()) {
+            (None, None) => None,
+            (f, s) => Some((f, s)),
+        }
     }
-  }
 
-  #[inline]
-  fn size_hint(&self) -> (usize, Option<usize>) {
-    let (a_lower, a_upper) = self.a.size_hint();
-    let (b_lower, b_upper) = self.b.size_hint();
+    #[inline]
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let (a_lower, a_upper) = self.a.size_hint();
+        let (b_lower, b_upper) = self.b.size_hint();
 
-    let lower = cmp::min(a_lower, b_lower);
+        let lower = cmp::min(a_lower, b_lower);
 
-    let upper = match (a_upper, b_upper) {
-      (Some(x), Some(y)) => Some(cmp::max(x,y)),
-      (Some(x), None) => Some(x),
-      (None, Some(y)) => Some(y),
-      (None, None) => None
-    };
+        let upper = match (a_upper, b_upper) {
+            (Some(x), Some(y)) => Some(cmp::max(x, y)),
+            (Some(x), None) => Some(x),
+            (None, Some(y)) => Some(y),
+            (None, None) => None,
+        };
 
-    (lower, upper)
-  }
+        (lower, upper)
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,15 @@
-#![deny(missing_docs,trivial_casts,trivial_numeric_casts,
-        missing_debug_implementations, missing_copy_implementations,
-        unsafe_code,unused_import_braces,unused_qualifications)
-]
+#![deny(
+    missing_docs,
+    trivial_casts,
+    trivial_numeric_casts,
+    missing_debug_implementations,
+    missing_copy_implementations,
+    unsafe_code,
+    unused_import_braces,
+    unused_qualifications
+)]
 // Copyright 2015-2017 Daniel P. Clark & array_tool Developers
-// 
+//
 // Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
 // http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
 // http://opensource.org/licenses/MIT>, at your option. This file may not be
@@ -30,13 +36,12 @@
 //! This crate is not limited to just Vector methods and has some helpful
 //! string methods as well.
 
-
-/// Array Tool provides useful methods for iterators 
+/// Array Tool provides useful methods for iterators
 pub mod iter;
-/// Array Tool provides many useful methods for vectors
-pub mod vec;
 /// A string is a collection so we should have more methods for handling strings.
 pub mod string;
+/// Array Tool provides many useful methods for vectors
+pub mod vec;
 
 /// Get `uniques` from two vectors
 ///
@@ -52,7 +57,6 @@ pub mod string;
 /// vec![vec![1,3,4], vec![6,7,8]]
 /// ```
 pub fn uniques<T: PartialEq + Clone>(a: Vec<T>, b: Vec<T>) -> Vec<Vec<T>> {
-  use self::vec::Uniq;
-  vec![a.uniq(b.clone()), b.uniq(a)]
+    use self::vec::Uniq;
+    vec![a.uniq(b.clone()), b.uniq(a)]
 }
-
